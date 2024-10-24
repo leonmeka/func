615ad9850acf5c56b899b207d70443709dd189da
Leon Meka <leonmeka@gmail.com>
2024-10-24 13:28:10 +0200

    chore: simplify examples

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import { FunctionConstructions } from "@core/utils";
-
 import { CoordinateSystem } from "@core/components/coordinate-system/coordinate-system";
 import { Grid } from "@core/components/grid/grid";
 import { Function } from "@core/components/primitives/function";
@@ -9,7 +7,6 @@ import { Plot } from "@core/components/plot/plot";
 export const App = () => {
   const f = (x: number) => x * Math.sin(x);
   const g = (x: number) => x * Math.cos(x);
-  const h = (x: number) => FunctionConstructions.SUBTRACT(f, g)(x);
 
   return (
     <div className="dark w-dvw h-dvh">
@@ -19,7 +16,6 @@ export const App = () => {
 
           <Function y={f} />
           <Function y={g} />
-          <Function y={h} />
         </Plot>
 
         <Debug />
